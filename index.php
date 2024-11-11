0d5590727563ec767d8de82822e2cd0970675a3b
Troy Chaplin <troy.chaplin@gmail.com>
2024-11-10 23:55:51 -0500

    change: clean up and add pattern example

--- index.php
+++ index.php
@@ -1,56 +1,19 @@
-<?php
-/**
- * The main template file
- *
- * This is the most generic template file in a WordPress theme
- * and one of the two required files for a theme (the other being style.css).
- * It is used to display a page when nothing more specific matches a query.
- * E.g., it puts together the home page when no home.php file exists.
- *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
- *
- * @package hybrid-theme
- */
-
-get_header();
-?>
-
+<?php get_header(); ?>
 	<main id="primary" class="site-main">
 
 		<?php
-		if ( have_posts() ) :
+		while ( have_posts() ) :
+			the_post();
 
-			if ( is_home() && ! is_front_page() ) :
-				?>
-				<header>
-					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-				</header>
-				<?php
+			get_template_part( 'template-parts/content', 'page' );
+
+			if ( comments_open() || get_comments_number() ) :
+				comments_template();
 			endif;
 
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
-
-				/*
-				 * Include the Post-Type-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_type() );
-
-			endwhile;
-
-			the_posts_navigation();
-
-		else :
-
-			get_template_part( 'template-parts/content', 'none' );
-
-		endif;
+		endwhile;
 		?>
-
-	</main><!-- #main -->
+	</main>
 
 <?php
 get_sidebar();
