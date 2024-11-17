970f85477f1c6b9522635cd4b5b8f4eb7af44402
Troy Chaplin <troy.chaplin@gmail.com>
2024-11-16 23:30:58 -0500

    add: theme supports

--- functions-hybrid.php
+++ functions-hybrid.php
@@ -139,3 +139,98 @@ function hybrid_add_template_parts_support() {
 	add_theme_support( 'block-template-parts' );
 }
 add_action( 'after_setup_theme', 'hybrid_add_template_parts_support' );
+
+
+/**
+ * Adds theme support for various features.
+ *
+ * This function is hooked to the 'after_setup_theme' action and adds support for:
+ * - Responsive embeds
+ * - Disabling custom colors
+ * - Disabling custom gradients
+ * - Disabling custom font sizes
+ *
+ * @return void
+ */
+function hybrid_add_theme_supports() {
+	add_theme_support( 'responsive-embeds' );
+	add_theme_support( 'disable-custom-colors' );
+	add_theme_support( 'disable-custom-gradients' );
+	add_theme_support( 'disable-custom-font-sizes' );
+}
+add_action( 'after_setup_theme','hybrid_add_theme_supports' );
+
+
+/**
+ * Adds theme support for various features and settings.
+ *
+ * This function adds support for:
+ * - Wide alignment
+ * - Custom line height
+ * - Custom spacing
+ * - Custom units (rem, em, px, vw, vh)
+ * - Editor color palette with predefined colors:
+ *   - Dusty Blue (#367cb3)
+ *   - Faded Brown (#7a6a53)
+ * - Editor gradient presets with predefined gradients:
+ *   - Dusty to Faded (linear-gradient(135deg, #367cb3 0%, #7a6a53 100%))
+ * - Editor font sizes with predefined sizes:
+ *   - Small (12px)
+ *   - Medium (18px)
+ *
+ * Note: This function cannot be used alongside a theme.json file.
+ *
+ * @package HybridTheme
+ */
+function hybrid_add_theme_supports_no_themejson() {
+	// Cannot use alongside a theme.json file
+	add_theme_support( 'align-wide' );
+	add_theme_support( 'custom-line-height' );
+	add_theme_support( 'custom-spacing' );
+
+	add_theme_support( 'custom-units', array( 'rem', 'em', 'px', 'vw', 'vh' ) );
+
+	add_theme_support(
+		'editor-color-palette',
+		array(
+			array(
+				'name'  => __( 'Dusty Blue', 'hybrid-theme' ),
+        'slug'  => 'dusty-blue',
+        'color' => '#367cb3',
+			),
+			array(
+				'name'  => __( 'Faded Brown', 'hybrid-theme' ),
+        'slug'  => 'faded-brown',
+        'color' => '#7a6a53',
+			),
+		)
+	);
+
+	add_theme_support(
+		'editor-gradient-presets',
+		array(
+			array(
+				'name'     => __( 'Dusty to Faded', 'hybrid-theme' ),
+				'gradient' => 'linear-gradient(135deg, #367cb3 0%, #7a6a53 100%)',
+				'slug'     => 'dusty-to-faded',
+			),
+		)
+	);
+
+	add_theme_support(
+		'editor-font-sizes',
+		array(
+			array(
+				'name' => 'Small',
+				'slug' => 'small',
+				'size' => 12
+			),
+			array(
+				'name' => 'Medium',
+				'slug' => 'medium',
+				'size' => 18
+			),
+		)
+	);
+}
+// add_action( 'after_setup_theme','hybrid_add_theme_supports_no_themejson' );
